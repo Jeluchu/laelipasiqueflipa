10084f876c76d7fbeaae85c412fc6f369c03ce76
Jeluchu <jesusmarcalderon@gmail.com>
2018-01-28 01:20:27 +0100

    LASTED UPDATE FOR 27/01/18

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 (function($) {
 
+	$(document).ready(function(){
+
+		$('.ir-arriba').click(function(){
+			$('body, html').animate({
+				scrollTop: '0px'
+			}, 300);
+		});
+
+		$(window).scroll(function(){
+			if( $(this).scrollTop() > 0 ){
+				$('.ir-arriba').slideDown(300);
+			} else {
+				$('.ir-arriba').slideUp(300);
+			}
+		});
+
+	});
+
 	skel
 		.breakpoints({
 			xlarge:	'(max-width: 1680px)',
